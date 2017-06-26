9e398662af6796cf8a8d5c5a900a9cec6af7db8f
AlanSMat <smather@astev.com>
2017-06-26 10:11:22 +1000

    test

---
+++ host_products/test_class.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<style type="text/css">
+.testClass {
+    border: 5px solid red;
+}      
+</style>
+</head>
+<body>
+<div class="testClass" xstyle="border: 1px solid #000;width:90px;">
+        aaa
+</div>
+</html>
